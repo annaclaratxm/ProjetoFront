b2c899f7dd5c5e84aab631acf412ff2aacfeeaec
annaclaratxm <annaclaratxm@gmail.com>
2024-06-03 17:28:54 -0300

    Tela de Login - Anna

--- src/components/Login/loginComponent.jsx
+++ src/components/Login/loginComponent.jsx
@@ -1,17 +1,24 @@
+import React from 'react';
+import './LoginComponent.css'; // Importando o arquivo CSS
+
 export default function LoginComponent() {
     return (
-        <div>
+        <div className="login-container">
             <div>
                 <h2>Login</h2>
             </div>
-            <div>
+            <div className="login-form">
                 <form>
                     <label>Usuário:</label>
-                    <input></input>
+                    <input type="text" />
                     <label>Senha:</label>
-                    <input></input>
-                    <input type="submit" value="Entrar"></input>
+                    <input type="password" />
+                    <a href="#" className="forgot-password-link">Esqueci minha senha</a>
+                    <input type="submit" value="Entrar" />
                 </form>
+                <div className="login-buttons">
+                    <button className="register-button">Registrar-se</button>
+                </div>
             </div>
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import LoginComponent from './components/Login/loginComponent.jsx'
+import LoginComponent from './components/Login/LoginComponent.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
