b2e0c9efdb4ba1797b3e1bb0e2ecc92cd7b20fcb
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-06-04 20:36:43 -0300

    Implementação inicial do Router

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <div>
+            <span>Início</span>
+            <span>Usuários</span>
+            <span>Serviços</span>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import LoginComponent from './components/Login/LoginComponent.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginComponent from './components/Login/loginComponent.jsx'
+import Home from './components/Home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginComponent />,
+  },
+  {
+    path: '/app',
+    element: <App />,
+    children: [
+      {
+        path: 'home',
+        element: <Home />,
+      },
+    ]
+  }
+])
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <LoginComponent />
+//   </React.StrictMode>,
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LoginComponent />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
