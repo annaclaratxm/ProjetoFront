381afcd49cb573507ae673129e8bb9df35e06296
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-06-04 23:44:41 -0300

    Ajustes no CSS

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import "./style.css";
 
 export default function Home() {
     return (
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quidem sint magnam odio nostrum atque repudiandae ullam dolores, et, error quod nulla deserunt natus. Ducimus laborum sequi voluptatum sed magnam!</p>
-)
+        <div className="home-div">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quidem sint magnam odio nostrum atque repudiandae ullam dolores, et, error quod nulla deserunt natus. Ducimus laborum sequi voluptatum sed magnam!</p>
+        </div>
+    )
 }
