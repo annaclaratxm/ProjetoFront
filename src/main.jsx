3d7fa405c820bfc5828f917bfcbe365d83fddf3e
annaclaratxm <annaclaratxm@gmail.com>
2024-06-16 18:07:53 -0300

    fix: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -28,52 +28,6 @@ const router = createBrowserRouter([
   }
 ])
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <LoginComponent />
-//   </React.StrictMode>,
-// )
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import CreateUser from './components/CrudUsers/CreateUser.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import LoginComponent from './components/Login/loginComponent.jsx'
-import Home from './components/Home/Home.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LoginComponent />,
-  },
-  {
-    path: '/app',
-    element: <App />,
-    children: [
-      {
-        path: 'home',
-        element: <Home />,
-      },
-      {
-        path: 'create-user',
-        element: <CreateUser />,
-      },
-    ]
-  }
-])
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <LoginComponent />
-//   </React.StrictMode>,
-// )
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
